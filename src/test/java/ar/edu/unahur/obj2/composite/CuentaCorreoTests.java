b0b717d9934e66e0b1e516a4f4e126d7252e9ce4
pabloandino0804-bit <pablo.andino0804@gmail.com>
2026-06-29 19:39:55 -0300

    test adicional en CuentaCorreoTests

--- src/test/java/ar/edu/unahur/obj2/composite/CuentaCorreoTests.java
+++ src/test/java/ar/edu/unahur/obj2/composite/CuentaCorreoTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unahur.obj2.composite.filtros.FiltroTamaño;
+import ar.edu.unahur.obj2.composite.filtros.filtroscompuestos.FiltroNot;
 
 public class CuentaCorreoTests {
     @Test
@@ -15,6 +16,18 @@ public class CuentaCorreoTests {
         Correo correo4 = new Correo("clase 4 presencial", 199, "cuarta semana de clases");
 
         CuentaCorreo miCuenta = new CuentaCorreo(new FiltroTamaño(200));
+
+        assertEquals(miCuenta.solicitarEmails(correo1, correo2, correo3, correo4).size(), 2);
+    }
+
+    @Test
+    void queDevuelvaUnaListaDe2ElementosPorUnaCuentaConFiltroTamañoNotMayorA200() {
+        Correo correo1 = new Correo("Junta importante por inicio de actividades", 220, "Reunion necesaria");
+        Correo correo2 = new Correo("junta 2", 213, "segundo reunion");
+        Correo correo3 = new Correo("junta de amigos", 140, "tercer reunion");
+        Correo correo4 = new Correo("clase 4 presencial", 199, "cuarta semana de clases");
+
+        CuentaCorreo miCuenta = new CuentaCorreo(new FiltroNot(new FiltroTamaño(200)));
 
         assertEquals(miCuenta.solicitarEmails(correo1, correo2, correo3, correo4).size(), 2);
     }
