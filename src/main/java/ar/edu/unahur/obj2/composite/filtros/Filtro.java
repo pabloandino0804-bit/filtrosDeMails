32f8b5e84a157e08676f38d6c5766e763473e6c0
pabloandino0804-bit <pablo.andino0804@gmail.com>
2026-07-30 11:04:50 -0300

    Reiniciando filtros compuestos

--- src/main/java/ar/edu/unahur/obj2/composite/filtros/Filtro.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unahur.obj2.composite.filtros;
-
-import ar.edu.unahur.obj2.composite.Correo;
-
-public interface Filtro {
-
-    Boolean cumple(Correo correo);
-
-}
--- src/main/java/ar/edu/unahur/obj2/composite/filtros/filtrosCompuestos/FiltroCompuesto.java
+++ src/main/java/ar/edu/unahur/obj2/composite/filtros/filtrosCompuestos/FiltroCompuesto.java
@@ -1,29 +1,5 @@
 package ar.edu.unahur.obj2.composite.filtros.filtrosCompuestos;
 
-import java.util.List;
+public class FiltroCompuesto {
 
-import ar.edu.unahur.obj2.composite.filtros.IFiltro;
-
-public abstract class FiltroCompuesto implements IFiltro{
-
-    protected List<IFiltro> filtros;
-
-    public FiltroCompuesto(List<IFiltro> filtros) {
-        this.filtros = filtros;
-    }
-
-    @Override
-    public Boolean aplicar() {
-        return doAplicar();
-    } 
-
-    public abstract Boolean doAplicar(); 
-
-    public void agregarFiltro(IFiltro filtro){
-        filtros.add(filtro);
-    }
-
-    public void vaciarFiltros(){
-        filtros.clear();
-    }
 }
